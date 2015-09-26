b6307d198edc5621eb50dc23afe1a0f5055ea246
MaksimDragun <MaksimDragun@live.com>
2015-09-27 00:26:54 +0300

    #9 List page

--- database-web/src/main/java/by/bsuir/drahun/database/controller/DatabaseController.java
+++ database-web/src/main/java/by/bsuir/drahun/database/controller/DatabaseController.java
@@ -1,18 +1,32 @@
 package by.bsuir.drahun.database.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import by.bsuir.drahun.database.business.OfferService;
+import by.bsuir.drahun.database.domain.ProductOffer;
+import by.bsuir.drahun.database.query.ProductOfferQuery;
 
 @Controller
 public class DatabaseController {
+	
+	@Autowired
+	private OfferService offerServise;
 	
 	@RequestMapping("/")
 	public String getDatabasePage() {
 		return "index";
 	}
 	@RequestMapping("/offer-list")
-	public String getOfferList() {
-		return "offer-list";
+	public ModelAndView getOfferList(ModelAndView model) {
+		List<ProductOffer> offerList = offerServise.fetchOffers(new ProductOfferQuery());
+	    model.addObject("offerList", offerList);
+	    model.setViewName("offer-list");
+		return model;
 	}
-
+	
 }
--- database-web/src/main/webapp/WEB-INF/views/offer-list.jsp
+++ database-web/src/main/webapp/WEB-INF/views/offer-list.jsp
@@ -6,5 +6,22 @@
 </head>
 <body>
 	Offer List
+	<table border="1">
+		<th>No</th>
+		<th>Product code</th>
+		<th>Product title</th>
+		<th>Cost</th>
+		<th>Quantity</th>
+
+		<c:forEach var="offer" items="${offerList}" varStatus="status">
+			<tr>
+				<td>${status.index + 1}</td>
+				<td>${offer.product.productCode}</td>
+				<td>${offer.product.productTitle}</td>
+				<td>${offer.cost}</td>
+				<td>${offer.quantity}</td>
+			</tr>
+		</c:forEach>
+	</table>
 </body>
 </html>
